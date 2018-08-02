60e167a8ce93bf20b285d873724ee6498dc0c47c
ycxzuo <343720929@qq.com>
2018-08-02 21:58:18 +0800

    first commit

---
+++ spring-cloud-config/spring-cloud-config-client/src/main/java/com/yczuoxin/springcloud/config/client/SpringCloudConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.yczuoxin.springcloud.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+    }
+}
---
+++ spring-cloud-config/spring-cloud-config-server/src/main/java/com/yczuoxin/springcloud/config/server/SpringCloudConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.yczuoxin.springcloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
+}
