ee7bbd10c531ffe6fe01608489a05a501db2bed8
ycxzuo <343720929@qq.com>
2018-08-14 23:22:48 +0800

    提交SC服务端和客户端架构

---
+++ spring-cloud-config/spring-cloud-client-application/src/test/java/com/yczuoxin/springcloud/client/application/SpringCloudClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yczuoxin.springcloud.client.application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ spring-cloud-config/spring-cloud-server-application/src/main/java/com/yczuoxin/springcloud/server/application/SpringCloudServerApplication.java
@@ -0,0 +1,12 @@
+package com.yczuoxin.springcloud.server.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServerApplication.class, args);
+    }
+}
