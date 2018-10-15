935a2b4ba580e835fb436a6e6d1b2b9935ada231
zuoxin <343720929@qq.com>
2018-10-15 18:06:55 +0800

    新增zookeeper的注册发现及为提交的文件

---
+++ zookeeper-client/src/test/java/com/yczuoxin/zookeeperclient/ZookeeperClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yczuoxin.zookeeperclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZookeeperClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
