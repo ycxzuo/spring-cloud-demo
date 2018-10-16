87bcf5db77c30ada0f32c9f8bcdc0668043ed559
ycxzuo <343720929@qq.com>
2018-10-16 20:14:34 +0800

    修改了feign的名称

--- consul/consul-application/src/main/java/com/yczuoxin/consulapplication/client/ConsulClient.java
+++ consul/consul-application/src/main/java/com/yczuoxin/consulapplication/client/ConsulClient.java
@@ -3,7 +3,7 @@ package com.yczuoxin.consulapplication.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("consul-service")
+@FeignClient("consul-client")
 public interface ConsulClient {
 
     @GetMapping(value = "/demo")
--- consul/consul-client/src/main/java/com/yczuoxin/consulclient/service/ConsulService.java
+++ consul/consul-client/src/main/java/com/yczuoxin/consulclient/service/ConsulService.java
@@ -3,7 +3,7 @@ package com.yczuoxin.consulclient.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("consul-service")
+@FeignClient("consul-client")
 public interface ConsulService {
 
     @GetMapping("/demo")
