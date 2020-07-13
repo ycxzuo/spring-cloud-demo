dcedf92e070b8675eef1161758699357a4746fe8
yczuoxin <343720929@qq.com>
2020-07-13 21:55:02 +0800

    Test WebFlex demo

--- eureka-server/src/main/java/com/yczuoxin/demo/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/yczuoxin/demo/eurekaserver/EurekaServerApplication.java
@@ -1,8 +1,11 @@
 package com.yczuoxin.demo.eurekaserver;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerApplicationContext;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +13,13 @@ public class EurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(WebServerApplicationContext context) {
+        return args -> {
+            System.out.println("当前 WebServer 实现类为："  + context.getWebServer().getClass().getName());
+        };
     }
 
 }
--- eureka-server/src/main/java/com/yczuoxin/demo/eurekaserver/controller/TestController.java
+++ eureka-server/src/main/java/com/yczuoxin/demo/eurekaserver/controller/TestController.java
@@ -1,7 +1,14 @@
 package com.yczuoxin.demo.eurekaserver.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
+import reactor.core.publisher.Mono;
+
 
 @RestController
 public class TestController {
@@ -10,7 +17,13 @@ public class TestController {
     public void close(){
         System.out.println("closing");
         // 测试
-        System.exit(0);
+        System.exit(-1);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> helloWorld() {
+        return RouterFunctions.route(RequestPredicates.GET("/hello-world"), request -> ServerResponse.ok().body(Mono.just("hello")));
     }
 
 }
+
