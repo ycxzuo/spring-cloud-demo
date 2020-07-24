163bcea8dab83b65e95d98a2e0edab1b947d88d0
yczuoxin <343720929@qq.com>
2020-07-24 14:39:46 +0800

    提交部分项目结构

---
+++ nacos-demo/nacos-configuration/src/main/java/com/yczuoxin/nacosconfiguration/NacosConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.yczuoxin.nacosconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigurationApplication.class, args);
+    }
+
+}
---
+++ nacos-demo/nacos-configuration/src/test/java/com/yczuoxin/nacosconfiguration/NacosConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yczuoxin.nacosconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
