29f15ac3316a90c954ee37c580b2800695e0f67a
yczuoxin <343720929@qq.com>
2020-08-02 23:15:23 +0800

    编程思想的示例代码

---
+++ webflux-demo-2/src/main/java/com/yczuoxin/config/WebConfiguration.java
@@ -0,0 +1,42 @@
+package com.yczuoxin.config;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+
+//@Configuration
+@SpringBootApplication
+//@EnableAutoConfiguration
+public class WebConfiguration {
+
+    @Bean
+    public RouterFunction<ServerResponse> helloWorld() {
+        return route(GET("hello-world"), request -> ok().body(Mono.just("Hello, World!"), String.class));
+    }
+
+    @EventListener(WebServerInitializedEvent.class)
+    public void onWebServerReady(WebServerInitializedEvent event) {
+        System.out.println("WebConfiguration: 当前 WebServer 实现类为" + event.getWebServer().getClass().getName());
+    }
+
+    @Bean
+    public ApplicationRunner runner(WebServerApplicationContext context) {
+        return args -> {
+            System.out.println("当前 HelloWorld Bean 实现类为" + context.getBean("helloWorld").getClass().getName());
+            System.out.println("当前 WebConfiguration Bean 实现类为" + context.getBean(WebConfiguration.class).getClass().getName());
+        };
+    }
+
+}
--- webflux-demo-2/src/main/java/com/yczuoxin/webfluxdemo2/WebfluxDemo2Application.java
+++ webflux-demo-2/src/main/java/com/yczuoxin/webfluxdemo2/WebfluxDemo2Application.java
@@ -1,13 +1,35 @@
 package com.yczuoxin.webfluxdemo2;
 
+import com.yczuoxin.config.WebConfiguration;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 
-@SpringBootApplication
+//@SpringBootApplication(scanBasePackages = "com.yczuoxin.config")
+//@Configuration
+//@ComponentScan
+//@EnableAutoConfiguration
 public class WebfluxDemo2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebfluxDemo2Application.class, args);
+        SpringApplication.run(WebConfiguration.class, args);
+    }
+
+//    @Bean
+//    public ApplicationRunner runner(WebServerApplicationContext context) {
+//        return args -> System.out.println("当前 WebServer 实现类为" + context.getWebServer().getClass().getName());
+//    }
+
+    @EventListener(WebServerInitializedEvent.class)
+    public void onWebServerReady(WebServerInitializedEvent event) {
+        System.out.println("WebfluxDemo2Application: 当前 WebServer 实现类为" + event.getWebServer().getClass().getName());
     }
 
 }
