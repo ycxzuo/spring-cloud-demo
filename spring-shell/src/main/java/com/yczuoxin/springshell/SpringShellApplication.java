b6288fa91043525b3808edcc652c933c23f4b6dc
ycxzuo <343720929@qq.com>
2025-07-31 23:28:02 +0800

    增加springshell的能力

---
+++ spring-shell/src/main/java/com/yczuoxin/springshell/SpringShellApplication.java
@@ -0,0 +1,13 @@
+package com.yczuoxin.springshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShellApplication.class, args);
+    }
+
+}
